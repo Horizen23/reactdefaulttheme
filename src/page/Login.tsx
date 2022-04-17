354545c0359173d1b573b3fa7c56e029fac1aa29
Horizen23 <com100pb@gmail.com>
2022-04-17 23:13:44 +0700

    setloadding

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components/macro';
 import { useDeviseAuth } from '../features/user/hook';
 import api from '../services/Api';
+import { ThemedText } from '../theme';
 
 const Culom = styled.div`
     ${({theme})=>theme.flexColumnNoWrap}
@@ -14,10 +15,12 @@ const Login = () => {
     const [onLogin] = useDeviseAuth();
     const inputuser = useRef<HTMLInputElement>(null)
     const inputpassword = useRef<HTMLInputElement>(null)
+    const [loading,setLoading] = useState(false)
     useEffect(()=>{
             inputuser.current?.focus()
     },[])
     const loginfn = async () =>{
+        setLoading(true)
        let res =  await  api.login(inputuser.current?.value as string, inputpassword.current?.value as string)
        if(res.status === 200){
         onLogin({
@@ -29,7 +32,7 @@ const Login = () => {
             username:res.data.username 
         })
        }
-       console.log(res)
+       setLoading(false)
     }
     console.log('rereander')
     return (
@@ -37,6 +40,9 @@ const Login = () => {
             <input type="text" ref={inputuser} />
             <input type="password" ref={inputpassword} />
             <button onClick={loginfn}>login</button>
+            {
+                loading&&<ThemedText.Label>loadding</ThemedText.Label>
+            }
         </Culom>
     );
 };
